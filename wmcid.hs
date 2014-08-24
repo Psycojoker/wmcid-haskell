a38fdd06b50db8e7104cacdb064e358579a351c8
Laurent Peuch <cortex@worlddomination.be>
2014-08-24 11:38:28 +0200

    [mod] move to a custom data for return of availableService

--- wmcid.hs
+++ wmcid.hs
@@ -2,11 +2,16 @@ import Data.Maybe(fromMaybe)
 import System.IO(hGetContents)
 import System.Process(proc, createProcess, StdStream(CreatePipe), std_out, std_err, waitForProcess)
 
-availableServices :: IO [(Bool, String)]
+data Service = Service {
+        name :: String,
+        running :: Bool
+    } deriving Show
+
+availableServices :: IO [Service]
 availableServices = do
     (_, Just hout, Just herr, pid) <- createProcess (proc "service" ["--status-all"]){ std_out = CreatePipe, std_err = CreatePipe }
     waitForProcess pid
     services <- hGetContents hout
-    return $ map (\line -> (if (line !! 3) == '+' then True else False, drop 8 line)) (lines services)
+    return $ map (\line -> Service (drop 8 line) (if (line !! 3) == '+' then True else False)) (lines services)
 
 main = putStrLn "pouet"
